57f1e29cc55d9b3e65c5e6a12eace4a802f4d1c4
Minh10 <36910006+Minh10@users.noreply.github.com>
2018-09-27 10:20:52 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,14 +1,19 @@
 package week2.task1;
 
 public class Task1 {
-
-    public static int gcd(int a, int b) {
-        // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+    public static int gcd(int a, int b){
+        while(a != b){
+            if(a > b)   a = a - b;
+            if(a < b)   b = b - a;
     }
-
-    public static int fibonacci(int n) {
-        // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+    return a;
+}
+    public static int fibonacci(int n){
+        if(n == 1)   return 1;
+        else if(n == 2)   return 1;
+        else if(n > 2)    return(fibonacci(n - 2) + fibonacci(n - 1));
+        else {
+            System.out.println("n k ton tai");
+            return 1    ;}
     }
 }
