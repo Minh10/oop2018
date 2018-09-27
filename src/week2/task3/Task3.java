1c17bc636212f5243126b6a3cf79ef6e6308d4b3
Minh10 <36910006+Minh10@users.noreply.github.com>
2018-09-27 10:22:17 +0700

    Update Task3.java

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -1,6 +1,60 @@
-package week2.task3;
+package week2.Company;
 
-public class Task3 {
+  public class Company {
+    private String name;
+    private int product;// so luong san pham
+    private float cost;// gia 1 san pham;
+    private float salary;//tien luong
+
+    public void setProduct(int product) {
+        this.product = product;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(float monney) {
+        this.salary = monney;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public int getProduct() {
+        return product;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+    public  Company(){
+        name = "Minh";
+        product = 100;
+        cost = 10;
+        salary = 5;
+    }
+    public Company(String name, int product, float cost, float monney){
+        this.name = name;
+        this.product = product;
+        this.cost = cost;
+        this.salary = monney;
+    }
+    public void info(){
+        System.out.println(name + " " + salary);
+    }
+
+}
+
 }
 
 //TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
