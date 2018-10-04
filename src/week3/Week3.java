7857b8842c99058fdcf14d5c7166d7c3ae8d2a6f
Minh10 <36910006+Minh10@users.noreply.github.com>
2018-10-04 07:39:34 +0700

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -1,25 +1,36 @@
 package week3;
 
 public class Week3 {
-
     public static int max(int m, int n) {
-        // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        // todo: tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
+        if(m > n)   return m;
+        else
+            return n;
     }
 
     public static int minOfArray(int[] array) {
-        // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        // todo: tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
+        int min = array[0];
+        for(int i = 1; i < array.length; i++){
+            if( array[i] < min ) min = array[i];
+        }
+        return min;
     }
 
     /**
-     * Chương trình tính chỉ số BMI và in ra kết quả đánh giá
+     * chương trình tính chỉ số bmi và in ra kết quả đánh giá
      * @param weight cân nặng
      * @param height chiều cao
-     * @return Thiếu cân, Bình thường, Thừa cân, Béo phì
+     * @return thiếu cân, bình thường, thừa cân, béo phì
      */
-    public static String calculateBMI(double weight, double height) {
-        // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
+    public static String calculatebmi(double weight, double height) {
+        // todo: viết chương trình tính chỉ số bmi và in ra kết quả đánh giá
+        double bmi;
+        bmi = weight/(height*height);
+        if(bmi < 18.5)  return"thieu can";
+        if(bmi >= 18.5 && bmi <= 24.99) return"binh thuong";
+        if(bmi >= 25 && bmi <30) return"thua can";
+        else if(bmi > 30) return"beo phi";
         return null;
     }
 }
