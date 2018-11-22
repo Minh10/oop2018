900b1c306e29f982c0b4cd2a228076179d0887a5
Minh10 <36910006+Minh10@users.noreply.github.com>
2018-11-22 10:15:53 +0700

    Update Task1.java

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,38 @@
-package week11;
 
-public class Task1 {
+public class task1 <T extends Comparable<T>> {
+
+    private void exch(T a[], int i, int j) {
+        T swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    public void sort(T arr[]){
+        for(int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i].compareTo(arr[j]) > 0) {
+                    exch(arr, i, j);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args){
+        task1 t = new task1();
+
+        Integer arr[] = {1, 2 ,3 ,4, 0};
+
+        t.sort(arr);
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        Double a[] = {4.5, 5.6, 1.3};
+        t.sort(a);
+        for(int i = 0; i < a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+    }
+
 }
