3c2ced4a110554e49de26195ba0f12d2f581ef9e
Minh10 <36910006+Minh10@users.noreply.github.com>
2018-11-22 17:34:43 +0700

    Update Task2.java

--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,28 @@
 package week11;
 
-public class Task2 {
+import java.util.Arrays;
+
+public class Task2<T extends Comparable<T>> {
+    public T findMax(T arr[]) {
+        T max = arr[0];
+        for(int i = 1; i < arr.length; ++i)
+            if (arr[i].compareTo(max) >= 0)
+                max = arr[i];
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Task2 t2 = new Task2();
+
+
+        Integer intArr[] = {3, 4, 1, 5};
+        System.out.println(Arrays.toString(intArr));
+        System.out.println("Max: " + t2.findMax(intArr));
+
+        Double doubleArr[] = {3.05, 4.6, 4.25, 5.45};
+        System.out.println(Arrays.toString(doubleArr));
+        System.out.println("Max: " + t2.findMax(doubleArr));
+
+    }
 }
